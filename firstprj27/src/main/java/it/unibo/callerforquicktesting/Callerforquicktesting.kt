ca69be095e9d4905ca5c4386963de631b3b46fea
Antonio Natali <agoognat@gmail.com>
2026-09-04 17:33:29 +0200

    Done CallerTcpWithChart

--- firstprj27/src/main/java/it/unibo/callerforquicktesting/Callerforquicktesting.kt
+++ firstprj27/src/main/java/it/unibo/callerforquicktesting/Callerforquicktesting.kt
@@ -50,7 +50,7 @@ class Callerforquicktesting ( name: String, scope: CoroutineScope, isconfined: B
 						 	   
 						if( checkMsgContent( Term.createTerm("values(S)"), Term.createTerm("values(S)"), 
 						                        currentMsg.msgContent()) ) { //set msgArgList
-								 val ReceivedString = payloadArg(0)                                      
+								 val ReceivedString = payloadArg(0)                                       
 								 val chartUrl       = MyCode.ChartUtils.buildMapChartUrl(ReceivedString)  
 								  MyCode.ChartUtils.OpenChartInBrowser(chartUrl)                          
 						}
