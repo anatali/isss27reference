2a9b5e807291ce84c6326d0e65d4411279b9f0f7
Antonio Natali <agoognat@gmail.com>
2026-09-17 12:03:14 +0200

    starting

---
+++ demoprologqak/demoprologqak.pl
@@ -0,0 +1,7 @@
+%====================================================================================
+% demoprologqak description   
+%====================================================================================
+%====================================================================================
+context(ctxfdemoprolog, "localhost",  "TCP", "8220").
+ qactor( actorusingprolog, ctxfdemoprolog, "it.unibo.actorusingprolog.Actorusingprolog").
+ static(actorusingprolog).
